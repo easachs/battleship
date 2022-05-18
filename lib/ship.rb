86677390cc4a5f2f7209790b8d6783a0aeeeb94d
Kevin Ta <kvnta@yahoo.com>
2022-05-18 08:20:21 -0600

    Complete ship spec and class

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,14 +1,15 @@
 class Ship
-  attr_reader = :name, :length
+  attr_reader  :name, :length, :health
 
   def initialize (name, length)
     @name = name
     @length = length
     @health = length
+    @sunk = false
   end
 
   def sunk?
-   @health == 0
+  @sunk = @health == 0
   end
 
   def hit
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,21 +1,42 @@
 require './lib/ship.rb'
 
 RSpec.describe Ship do
+
   it 'exists' do
     cruiser = Ship.new("Cruiser", 3)
-    expect(cruiser).to be_a(Ship)
+    expect(cruiser).to be_instance_of(Ship)
   end
-  it "cruiser has attributes" do 
+
+  it "has a name" do
     cruiser = Ship.new("Cruiser", 3)
-    sunk = Ship.new("Cruiser", 3)
+    name = Ship.new("Cruiser", 3)
+    expect(cruiser.name).to eq("Cruiser")
+  end
+
+  it "has a length" do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.length).to eq(3)
+  end
+
+  it 'has health' do
+    cruiser = Ship.new("Cruiser", 3)
+    expect(cruiser.health).to eq(3)
+  end
+
+  it "is sunk" do
+    cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.sunk?).to eq(false)
   end
 
   it "when hit" do
     cruiser = Ship.new("Cruiser", 3)
-    sunk = Ship.new("Cruiser", 3)
-    hit = Ship.new("Cruiser", 2)
-    expect(cruiser.hit).to eq(2)
+    cruiser.hit
+    expect(cruiser.health).to eq(2)
+    cruiser.hit
+    expect(cruiser.health).to eq(1)
+    expect(cruiser.sunk?).to eq(false)
+    cruiser.hit
+    expect(cruiser.sunk?).to eq(true)
   end
 
 end
