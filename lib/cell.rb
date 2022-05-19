947df51665458415ce701ffc6c8dc85010267073
Kevin Ta <kvnta@yahoo.com>
2022-05-19 10:35:08 -0600

    Save current cell work

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,7 @@ class Cell
   def initialize (coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -13,5 +14,21 @@ class Cell
   def place_ship(ship)
     @ship = ship
   end
-  
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if !empty?
+      @ship.hit unless @fired_upon == true
+    end
+    @fired_upon = true
+  end
+
+  def render(status = false)
+    if !status then puts "S"
+  end
+  end
+#two main branches of render method
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,31 +2,64 @@ require './lib/ship'
 require './lib/cell'
 
 RSpec.describe Cell do
-  cell = Cell.new("B4")
-  it 'exists' do
-    expect(cell).to be_instance_of(Cell)
+  describe "initialize" do
+
+    cell = Cell.new("B4")
+    it 'exists' do
+      expect(cell).to be_instance_of(Cell)
+    end
+
+    it 'has coordinate' do
+      expect(cell.coordinate).to eq("B4")
+    end
+
+    it "has a cell ship" do
+      expect(cell.ship).to be_nil
+    end
+
+    it "has a cell empty" do
+      expect(cell.empty?).to eq(true)
+    end
+
+    it "has cruiser ship" do
+      cruiser = Ship.new("Cruiser", 3)
+      cell.place_ship(cruiser)
+      expect(cell.ship).to eq(cruiser)
+    end
+
+    it "place cruiser on cell" do
+      expect(cell.empty?).to be(false)
+    end
+
+    it "is fired upon?" do
+      expect(cell.fired_upon?).to be(false)
+    end
+
+    it "can be fired upon" do
+      cell.fire_upon
+      expect(cell.ship.health).to eq(2)
+      expect(cell.fired_upon?).to be(true)
+    end
+
+    it "cell can render" do
+      cell_1 = Cell.new("B4")
+      expect(cell_1.render).to eq(".")
+      cell_1.fire_upon
+      expect(cell_1.render).to eq("M")
+    end
+
+    it "another cell can render" do
+      cell_2 = Cell.new("C3")
+      cruiser = Ship.new("Cruiser", 3)
+      cell_2.place_ship(cruiser)
+      expect(cell.render).to be(".")
+      
+    end
   end
 
-  it 'has coordinate' do
-    expect(cell.coordinate).to eq("B4")
-  end
+  #describe "second part of test" do
 
-  it "has a cell ship" do
-    expect(cell.ship).to be_nil
-  end
 
-  it "has a cell empty" do
-    expect(cell.empty?).to eq(true)
-  end
-
-  it "has cruiser ship" do
-    cruiser = Ship.new("Cruiser", 3)
-    cell.place_ship(cruiser)
-    expect(cell.ship).to eq(cruiser)
-  end
-
-  it "place cruiser on cell" do
-    expect(cell.empty?).to be(false)
-  end
+  #end
 
 end
