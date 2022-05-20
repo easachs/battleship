da6f61f602a00452f3f03c7b0ce8b5def80d4125
Eli Sachs <easachs13@gmail.com>
2022-05-20 15:55:40 -0600

    Add Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,70 @@
 class Board
 
+  attr_reader :cells
+
+    def initialize
+      # @rows = ["A","B","C","D"]
+      # @columns = [1, 2, 3, 4]
+      @cells = {
+        "A1" => Cell.new("A1"),
+        "A2" => Cell.new("A2"),
+        "A3" => Cell.new("A3"),
+        "A4" => Cell.new("A4"),
+        "B1" => Cell.new("B1"),
+        "B2" => Cell.new("B2"),
+        "B3" => Cell.new("B3"),
+        "B4" => Cell.new("B4"),
+        "C1" => Cell.new("C1"),
+        "C2" => Cell.new("C2"),
+        "C3" => Cell.new("C3"),
+        "C4" => Cell.new("C4"),
+        "D1" => Cell.new("D1"),
+        "D2" => Cell.new("D2"),
+        "D3" => Cell.new("D3"),
+        "D4" => Cell.new("D4")
+      }
+    end
+
+    def valid_coordinate?(coordinate)
+      @cells.key?(coordinate)
+    end
+
+    def valid_placement?(ship, ship_coordinates)
+      # equal number of coordinates/ship length?
+      correct_length = ship_coordinates.length == ship.length
+
+      coordinate_pairs = ship_coordinates.each_cons(2)
+      # is ship on consecutive rows?
+      consecutive_rows = coordinate_pairs.all? do |pair|
+        pair[0][0] == pair[1][0] &&
+        pair[0][1].to_i + 1 == pair[1][1].to_i
+      end
+      # is ship on consecutive columns?
+      consecutive_columns = coordinate_pairs.all? do |pair|
+        pair[0][0].ord + 1 == pair[1][0].ord &&
+        pair[0][1] == pair[1][1]
+      end
+      # invalid if ship not on board
+      valid_coordinates = ship_coordinates.all? do |coordinate|
+        valid_coordinate?(coordinate)
+      end
+      # do ships overlap?
+      overlap = @cells.count do |k,v|
+        v.ship != nil
+      end
+
+      correct_length && valid_coordinates && overlap == 0 &&
+      (consecutive_rows || consecutive_columns)
+    end
+
+    def place(ship, ship_coordinates)
+      if valid_placement?(ship, ship_coordinates)
+        ship_coordinates.each do|coordinate|
+          @cells[coordinate].place_ship(ship)
+        end
+      else
+        nil
+      end
+    end
+
 end
