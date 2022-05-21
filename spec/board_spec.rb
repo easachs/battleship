dc828af3a194444235a004d34463d11e628452f5
Kevin Ta <kvnta@yahoo.com>
2022-05-20 21:16:13 -0600

    Finish board class and render method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -113,10 +113,12 @@ RSpec.describe Board do
   end
 
   describe "board render" do
-    xit 'renders game board' do
+    it 'renders game board' do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
       board.place(cruiser, ["A1", "A2", "A3"])
+      board.place(submarine, ["B4", "C4"])
       expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
       expect(board.render(true)).to eq("1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
     end
