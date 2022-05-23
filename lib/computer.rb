8efc5c80a6eb00a12ed2be7c8122beca1869b2e7
Eli Sachs <easachs13@gmail.com>
2022-05-23 16:32:04 -0600

    Refactor Computer class

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,13 +1,13 @@
 class Computer
 
   attr_reader :computer_board,
-              :computer_cruiser,
-              :computer_sub
+              :computer_ship_count
 
   def initialize
     @computer_board = nil
-    @computer_cruiser = nil
-    @computer_sub = nil
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_sub = Ship.new("Submarine", 2)
+    @computer_ship_count = 2
   end
 
   def create_board(height_input = 4, width_input = 4)
@@ -15,9 +15,6 @@ class Computer
   end
 
   def computer_placement
-    @computer_cruiser = Ship.new("Cruiser", 3)
-    @computer_sub = Ship.new("Submarine", 2)
-
     generated_coordinates = []
     loop = @computer_board.place(@computer_cruiser, generated_coordinates)
 
@@ -42,22 +39,32 @@ class Computer
     end
   end
 
-  # def computer_turn
-  #   computer_shot = player_board.cells.to_a.sample(1).flatten[0]
-  #   duplicate_shot = player_board.cells["#{computer_shot}"].render != "."
-  #   if !duplicate_shot
-  #     player_board.cells["#{computer_shot}"].fire_upon
-  #     if player_board.cells["#{computer_shot}"].render == "X"
-  #       puts "The computer sunk your #{player_board.cells["#{computer_shot}"].ship.name}!"
-  #       player_ship_count -= 1
-  #     elsif player_board.cells["#{computer_shot}"].render == "H"
-  #       puts "The computer's shot on #{computer_shot} was a hit."
-  #     elsif player_board.cells["#{computer_shot}"].render == "M"
-  #       puts "The computer's shot on #{computer_shot} was a miss."
-  #     end
-  #   else
-  #     computer_turn
-  #   end
-  # end
+  def player_turn
+    puts "Enter the coordinate for your shot:"
+    player_shot = gets.chomp.upcase
+    if @computer_board.valid_coordinate?(player_shot)
+      if @computer_board.cells["#{player_shot}"].fired_upon?
+        puts "You cannot fire upon the same cell twice."
+        player_turn
+      else
+        @computer_board.cells["#{player_shot}"].fire_upon
+        if @computer_board.cells["#{player_shot}"].render == "X"
+          puts "You sunk a #{@computer_board.cells["#{player_shot}"].ship.name}!"
+          @computer_ship_count -= 1
+        elsif @computer_board.cells["#{player_shot}"].render == "H"
+          puts "Your shot on #{player_shot} was a hit!"
+        elsif @computer_board.cells["#{player_shot}"].render == "M"
+          puts "Your shot on #{player_shot} was a miss."
+        end
+      end
+    else
+      puts "Invalid coordinate."
+      player_turn
+    end
+  end
+
+  def reset
+    @computer_ship_count = 2
+  end
 
 end
